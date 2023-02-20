c36d7a166eb70659a684338ffa402057f5ec0d15
Érick Santos <71834322+erarich@users.noreply.github.com>
2023-02-20 19:48:53 -0300

    Testing Material Design Button

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Button from "@mui/material/Button";
+
+function MyApp() {
+  return <Button variant="outlined">Hddddddddddddddrld</Button>;
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <p>Olá Mundo</p>
+    <MyApp/>
   </>
 )
